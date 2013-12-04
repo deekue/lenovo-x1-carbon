0b59f77c4d0e2d7526657910bce73198d276dd1e
Daniel Quinlan <daniel@chaosengine.net>
2013-12-03 17:05:54 -0800

    make deploy.sh work from wherever the git repo is

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-BASE_DIR=$HOME/src/lenovo-x1-carbon
+BASE_DIR=$(dirname -- "$0")
 cd $BASE_DIR
 
 find . -path ./.git -prune -o -type f -print | while read file ; do
   file=${file#.} 
   [ -e $file ] && continue
   [ "$file" == $(basename -- "$0") ] && continue
+  [ -d "$(dirname $file)" ] || sudo mkdir -p $(dirname $file)
   full_path=$BASE_DIR/$file 
   cd / 
   echo "[ln -svi $full_path $file]"
