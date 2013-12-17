8f676240c1a283d41fbb485f2124f2e62ee00b3f
Daniel Quinlan <daniel@chaosengine.net>
2013-12-17 15:14:53 -0800

    fix deploy.sh to use install instead of making symlinks   fixes problem with pm-powersave checking EUID of hook scripts

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 
-BASE_DIR=$(dirname -- "$0")
+BASE_DIR=$(readlink -f $(dirname -- "$0"))
 cd $BASE_DIR
 
 find . -path ./.git -prune -o -type f -print | while read file ; do
-  file=${file#.} 
-  [ -e $file ] && continue
+  file="${file#.}"
   [ "$file" == $(basename -- "$0") ] && continue
-  [ -d "$(dirname $file)" ] || sudo mkdir -p $(dirname $file)
-  full_path=$BASE_DIR/$file 
-  cd / 
-  echo "[ln -svi $full_path $file]"
-  sudo ln -svi $full_path $file 
-  cd - 
+  full_path="$BASE_DIR/$file"
+  sudo install -v -D -g root -o root -m 755 "$full_path" "$file"
 done
