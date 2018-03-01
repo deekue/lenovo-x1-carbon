c1990e92794058aad9c2d87a84a4f9a750ccbb31
Daniel Quinlan <daniel@chaosengine.net>
2018-02-28 17:32:38 -0800

    add hack to try to prevent thermal MCEs. BIOS sets RAPL MSR to 25W, lower that to 15W + bursts to 18.5W

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,22 @@
 #!/bin/bash 
 
 DEBUG=echo
+SKIP_FILE="/$(basename -- "$0")"
 BASE_DIR=$(readlink -f $(dirname -- "$0"))
 cd $BASE_DIR
 
 find . -path ./.git -prune -o -type f -print | while read file ; do
   file="${file#.}"
-  [ "$file" == /$(basename -- "$0") ] && continue
-  full_path="$BASE_DIR/$file"
-  $DEBUG sudo install --backup=off -v -b -D -g root -o root -m 755 "$full_path" "$file"
+  [ "$file" == "$SKIP_FILE"  ] && continue
+  src="$BASE_DIR/$file"
+  $DEBUG sudo install --backup=off -v -b -D -g root -o root -m 755 "$src" "$file"
+
+  # enable systemd service files
+  if [ "X${file##*.}" == "Xservice" ] ; then
+   if ! sudo systemctl list-unit-files | grep -qw $(basename -- "$file") ; then
+     echo "enabling systemd service $file"
+     $DEBUG systemctl enable $file
+   fi
+  fi
+
 done
